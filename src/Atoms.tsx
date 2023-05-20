aef63a8cf4d1709ac32ff7e9299b8d2ceacac7ae
kimhyemin9988 <rlagpals159@naver.com>
2023-05-20 16:28:18 +0900

    remove unnecessary atom
    user data atom

--- src/Atoms.tsx
+++ src/Atoms.tsx
@@ -26,17 +26,3 @@ export const Category = atom<ICategory[]>({
     default: [{ value: "Todo", label: "Todo" }, { value: "Done", label: "Done" }, { value: "Doing", label: "Doing" }],
     effects_UNSTABLE: [persistAtom],
 })
-
-export interface IuserData {
-    name: string;
-    email: string;
-    displayName: string;
-    phoneNumber: string;
-    photoURL: string;
-}
-
-export const UserData = atom<IuserData[]>({
-    key: "UserData",
-    default: [],
-    effects_UNSTABLE: [persistAtom],
-})
