10bf8b9e9dfcb9beb955d01e4ddb12b81dcbf96a
kimhyemin9988 <rlagpals159@naver.com>
2023-05-22 22:14:55 +0900

    Remove unnecessary atom

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -2,20 +2,15 @@ import { Navigate, Outlet, Route, redirect, useNavigate } from "react-router-dom
 import { authService } from "./todoFirebase";
 import { useEffect, useState } from "react";
 import { useSetRecoilState } from "recoil";
-import { UserData } from "./Atoms";
 
 const PrivateRoute = () => {
     //todo, category, profile 로그인시 접근 가능 page 보호
     const [inital, setinital] = useState(false); // 초기화
     const navigate = useNavigate();
-    const setTodosArray = useSetRecoilState(UserData);
 
     useEffect(() => {
         authService.onAuthStateChanged((user) => {
             user ?? navigate("/login");
-/*              setTodosArray((prev)=> prev = [{
-                displayName:user?.displayName,
-             }]); */
             setinital((prev) => prev = true);
         })
     }, [])
