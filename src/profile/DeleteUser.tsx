e82c7815c6313df63bca4ec4ea35cccc1a57e3d7
kimhyemin9988 <rlagpals159@naver.com>
2023-05-23 19:47:42 +0900

    When delete User, delete all data of the user.

--- src/profile/DeleteUser.tsx
+++ src/profile/DeleteUser.tsx
@@ -1,17 +1,53 @@
 import { deleteUser } from "firebase/auth";
 import { LargeBtnWhite } from "../AuthLogin";
 import { authService } from "../todoFirebase";
+import { useNavigate } from "react-router-dom";
+import { storage } from "../todoFirebase";
+import { ListResult, StorageReference, deleteObject, listAll, ref } from "firebase/storage";
 
 const DeleteUser = () => {
     const user = authService.currentUser;
+    const navigate = useNavigate();
+
+    const deleteUserFile = (response: ListResult) => {
+        response.items.forEach((itemRef: StorageReference) => {
+            try {
+                deleteObject(itemRef);
+            } catch (error: any) {
+                switch (error.code) {
+                    case "storage/unauthenticated":
+                        alert("사용자가 인증되지 않았습니다. 인증한 후 다시 시도해 보세요.");
+                        navigate('/login');
+                        return "사용자 재인증이 필요합니다.";
+                    default:
+                        return "파일 삭제를 실패하였습니다.";
+                }
+            }
+        });
+
+    }
+
+    const listAllFiles = async () => {
+        const desertRef = ref(storage, `${user?.uid}`);
+        const response = await listAll(desertRef);
+        if (response.items.length !== 0) {
+            deleteUserFile(response);
+        }
+
+    };
+
     const deleteUserFx = async () => {
+        alert("사용하고 계신 계정은 탈퇴할 경우 복구가 불가능합니다. 탈퇴 후 회원정보 및 이용기록은 모두 삭제됩니다.");
         try {
+            await listAllFiles();
             user && await deleteUser(user);
             alert("계정이 삭제되었습니다.");
         } catch (error: any) {
             // An error ocurred
             switch (error.code) {
                 case "auth/requires-recent-login":
+                    alert("사용자 재인증이 필요합니다");
+                    navigate('/login');
                     return "사용자 재인증이 필요합니다.";
                 case "auth/network-request-failed":
                     return "네트워크 연결에 실패 하였습니다.";
